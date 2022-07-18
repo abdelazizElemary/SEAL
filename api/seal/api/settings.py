811188dc535c50c3d2c8c1980c90aa1c28ccd891
Abdelaziz Elemary <abdelaziz528@gmail.com>
2022-07-18 03:51:38 +0200

    feat: remove django secret key

--- api/seal/api/settings.py
+++ api/seal/api/settings.py
@@ -13,15 +13,17 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+env = environ.Env()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+xbs08l(1b3*l&&uf8i5ku))41j_@x*l&a^#!%0d!2$($y+z+0'
+SECRET_KEY = env("DJANGO_SECRET_KEY", default="0")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
